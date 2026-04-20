c0d8eb7617a5999eb14a194dcc7e7a0df795733a
LevKorobeinikov <korobeinikoffl@yandex.ru>
2026-04-20 13:36:57 +0500

    change scheduler

--- scheduler/scheduler_service.py
+++ scheduler/scheduler_service.py
@@ -22,7 +22,7 @@ def build_scheduler(bot: Bot, bg_factory: BgManagerFactory) -> AsyncIOScheduler:
         run_transfer_job,
         trigger="cron",
         hour=13,
-        minute=40,
+        minute=45,
         id="run_transfer_job",
         max_instances=1,
     )
